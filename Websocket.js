446322adaa9c8572764f5c0956fd62c877be891d
kn <mackuang1@gmail.com>
2018-07-12 21:22:05 +0800

    init

---
+++ Websocket.js
@@ -0,0 +1,59 @@
+// import { mapActions, mapState } from "vuex";
+import Defer from "./Promise";
+class WebSocketImpt {
+  constructor() {
+    // this.url = "ws://192.168.100.81:9527/casualGame";
+    this.url = "ws://123.207.167.163:9010/ajaxchattest";
+    this.socket;
+    this.success;
+    this.error;
+    this.close;
+    //
+    this.acSet = {};
+  }
+  create(cb) {
+    this.socket = new WebSocket(this.url);
+    this.socket.onopen = () => {
+      console.log("WebSocket 建立连接成功!");
+      cb && cb();
+    };
+    this.socket.onmessage = event => {
+      console.log(event);
+      this.handleAc(JSON.parse(event.data));
+    };
+    this.socket.onerror = () => {
+      console.log("WebSocket 错误!");
+      this.error && this.error();
+    };
+    this.socket.onclose = () => {
+      console.log("WebSocket 关闭!");
+      this.close && this.close();
+    };
+  }
+  request(ac, data, cb) {
+    // console.log(object);
+    let fanfan = new Defer();
+    this.acSet[ac] = [false, fanfan];
+    this.socket.send(JSON.stringify(data));
+    return fanfan.promise;
+  }
+  on(ac, data, cb) {
+    let fanfan = new Defer();
+    this.acSet[ac] = [true, fanfan];
+    this.socket.send(JSON.stringify(data));
+    return fanfan.promise;
+  }
+  off(ac) {
+    delete this.acSet[ac];
+  }
+  handleAc(data) {
+    let item = this.acSet[data.ac];
+    if (item) {
+      item[1].resolve(data);
+      !item[0] && delete this.acSet[data.ac];
+    }
+    console.log(this.acSet);
+  }
+  sendMessage(data) {}
+}
+export default new WebSocketImpt();
